5a530c67b7b0ff8f31359a48a13ec74156794e2a
mul88-cyber <mulanto.ma88@gmail.com>
2025-07-20 20:31:32 +0700

    Update freqapp.py

--- freqapp.py
+++ freqapp.py
@@ -3,30 +3,28 @@ import pandas as pd
 import plotly.express as px
 
 # CONFIG
-SHEET_ID = "1ZcsGqqMzMLt35INSJMxwpAVKyTLzUwNiScVONELWSnQ"
-GID = "0"
-sheet_url = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=csv&gid={GID}"
+GCS_URL = "https://storage.googleapis.com/stock-csvku/hasil_saham_potensial.csv"
 
 st.set_page_config(page_title="Dashboard Saham Potensial", layout="wide")
 st.title("📊 Dashboard Saham Potensial")
 
 @st.cache_data(ttl=300)
 def load_data():
-    df = pd.read_csv(sheet_url)
+    df = pd.read_csv(GCS_URL)
     df['Last Trading Date'] = pd.to_datetime(df['Last Trading Date'], errors='coerce')
     df['Change %'] = (df['Change'] / df['Close']) * 100
     return df
 
 df = load_data()
 
-# Dropdown filter
+# Sidebar filter
 st.sidebar.header("📌 Filter")
 stock_list = sorted(df['Stock Code'].dropna().unique())
 selected_stock = st.sidebar.selectbox("Pilih Saham", stock_list)
 
 df_filtered = df[df['Stock Code'] == selected_stock].sort_values('Last Trading Date')
 
-# Show data table
+# Show table
 st.subheader(f"📈 Data Saham: {selected_stock}")
 st.dataframe(
     df_filtered[['Last Trading Date', 'Close', 'Change', 'Change %', 'Volume', 'Frequency']].sort_values('Last Trading Date', ascending=False),
@@ -34,7 +32,7 @@ st.dataframe(
     hide_index=True
 )
 
-# Tabs grafik
+# Tabs for visual
 tab1, tab2, tab3 = st.tabs(["📊 Harga Close", "📉 Volume", "📈 Change %"])
 
 with tab1:
@@ -49,5 +47,5 @@ with tab2:
 
 with tab3:
     fig3 = px.line(df_filtered, x='Last Trading Date', y='Change %',
-                   title=f"Persentase Perubahan Harga {selected_stock}", markers=True)
+                   title=f"Change % {selected_stock}", markers=True)
     st.plotly_chart(fig3, use_container_width=True)
